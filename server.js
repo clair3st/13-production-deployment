9fe145996b5cdc2cc95c621516e8b2f052fa25c9
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-06-22 11:21:04 -0700

    added new context

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express'),
+  requestProxy = requre('express-request-proxy'),
   port = process.env.PORT || 3000,
   app = express();
+
+var proxyGitHub = function(request, response) {
+  console.log('routing gitHub requestion for', request.params[0]);
+  (requestProxy({
+    url: 'https://api.github.com/' + request.params[0],
+    headers: {Authorization: 'token ' + process.env.GITHUB_TOKEN}
+  }))(request, response); //completes our context similar to an IFFE
+};
+
+app.get('/github/*', proxyGitHub);
 
 app.use(express.static('./'));
 
